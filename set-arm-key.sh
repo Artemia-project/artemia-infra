3804bd870c5791e155db873021b9ffc147e4d4b4
kibanana <kyw017763@gmail.com>
2025-07-28 00:00:19 +0900

    Add terraform management scripts

---
+++ set-arm-key.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+RESOURCE_GROUP_NAME=artemia-rg
+STORAGE_ACCOUNT_NAME=artemiastatestore
+CONTAINER_NAME=tfstate
+
+ACCOUNT_KEY=$(az storage account keys list --resource-group $RESOURCE_GROUP_NAME --account-name $STORAGE_ACCOUNT_NAME --query '[0].value' -o tsv)
+export ARM_ACCESS_KEY=$ACCOUNT_KEY
