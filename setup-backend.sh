f2cded2caaee01f3620e58ada8ee0497d61a8793
kibanana <kyw017763@gmail.com>
2025-07-28 13:47:54 +0900

    Update setup-backend.sh to use terraform instead of azure cli

--- setup-backend.sh
+++ setup-backend.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-RESOURCE_GROUP_NAME=artemia-rg
-STORAGE_ACCOUNT_NAME=artemiastatestore
-CONTAINER_NAME=tfstate
+# RESOURCE_GROUP_NAME=artemia-rg
+# STORAGE_ACCOUNT_NAME=artemiastatestore
+# CONTAINER_NAME=tfstate
 
-# Create resource group
-az group create --name $RESOURCE_GROUP_NAME --location koreacentral
+# # Create resource group
+# az group create --name $RESOURCE_GROUP_NAME --location koreacentral
 
-# Create storage account
-az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services blob
+# # Create storage account
+# az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services blob
 
-# Create blob container
-az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME
+# # Create blob container
+# az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME
+
+terraform apply -target=azurerm_storage_account.res-114 -auto-approve
