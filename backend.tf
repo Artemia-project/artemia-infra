d1ee478818a8d005806182de0f34c062af36a1c2
kibanana <kyw017763@gmail.com>
2025-07-30 07:35:13 +0900

    Update backend container name

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "artemia-rg"
     storage_account_name = "artemiastatestore"
-    container_name       = "tfstate"
+    container_name       = "terraform-state"
     key                  = "terraform.tfstate"
   }
 }
