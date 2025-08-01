50917d65594ed1427f6582dec8d55eb5a7bdb9d8
kibanana <kyw017763@gmail.com>
2025-08-02 00:16:23 +0900

    Update set-arm-key.sh to update resource name

--- set-arm-key.sh
+++ set-arm-key.sh
@@ -2,7 +2,7 @@
 
 RESOURCE_GROUP_NAME=artemia-rg
 STORAGE_ACCOUNT_NAME=artemiastatestore
-CONTAINER_NAME=tfstate
+CONTAINER_NAME=terraform-state
 
 ACCOUNT_KEY=$(az storage account keys list --resource-group $RESOURCE_GROUP_NAME --account-name $STORAGE_ACCOUNT_NAME --query '[0].value' -o tsv)
 export ARM_ACCESS_KEY=$ACCOUNT_KEY
